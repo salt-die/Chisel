99dcd0455aa864e756c4e10b1617bf6e5e16649c
salt-die <naclmolecule@gmail.com>
2020-02-09 18:55:08 -0600

    ToolButton added to __init__

--- chisel/widgets/__init__.py
+++ chisel/widgets/__init__.py
@@ -1,4 +1,4 @@
 from .cursor import Cursor  # noqa: F401
 from .options import OptionsPanel  # noqa: F401
-from .buttons import BurgerButton  # noqa: F401
+from .buttons import BurgerButton, ToolButton  # noqa: F401
 from .core.chisel import Chisel  # noqa: F401
